8de969d38b094f66593c42903ea17aabf5b72ad8
Straad <estrada.meraz@gmail.com>
2019-04-22 01:08:52 -0500

    Starting project with ASP.NET MVC

---
+++ Hector_de_Leon_Curso_de MVC_Net_CSharp/Exercise0001/Controllers/AccessController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Exercise0001.Models;
+
+namespace Exercise0001.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Enter(string user, string password)
+        {
+            try
+            {
+                using (Example001Entities dbContext = new Example001Entities())
+                {
+                    var lst = from d in dbContext.users
+                              join s in dbContext.cstates on d.idState equals s.id
+                              where d.username.Equals(user) && d.password.Equals(password) && s.name.Equals("Active")
+                              select d;
+
+                    if (lst.Count() == 1)
+                    {
+                        Session["User"] = lst.First();
+                        return Content("1");
+                    }
+                }
+
+                return Content("Invalid user.");
+            }
+            catch (Exception ex)
+            {
+                return Content("Ocurred an error: " + ex.Message);
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
