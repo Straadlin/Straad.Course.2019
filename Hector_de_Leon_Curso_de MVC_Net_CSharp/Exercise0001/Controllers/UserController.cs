e774b0bb513304ae5dfc9947338786ad26b6061c
Straad <estrada.meraz@gmail.com>
2019-04-25 01:48:41 -0500

    CRUD with validations and add function.

--- Hector_de_Leon_Curso_de MVC_Net_CSharp/Exercise0001/Controllers/UserController.cs
+++ Hector_de_Leon_Curso_de MVC_Net_CSharp/Exercise0001/Controllers/UserController.cs
@@ -7,6 +7,7 @@
     using System.Web.Mvc;
     using Exercise0001.Models;
     using Exercise0001.Models.TablesViewModels;
+    using Exercise0001.Models.ViewModels;
 
     public class UserController : Controller
     {
@@ -17,18 +18,52 @@
 
             using (Example001Entities dbContext = new Example001Entities())
             {
-                lst = (from d in dbContext.users join s in dbContext.cstates on d.idState equals s.id
-                      select new UserTableViewModel
-                      {
-                          Id = d.id,
-                          Name = d.name,
-                          UserName = d.username,
-                          Password = d.password
-                      }).ToList();
+                lst = (from d in dbContext.users
+                       join s in dbContext.cstates on d.idState equals s.id
+                       select new UserTableViewModel
+                       {
+                           Id = d.id,
+                           Name = d.name,
+                           UserName = d.username,
+                           Password = d.password
+                       }).ToList();
 
             }
 
             return View(lst);
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(UserViewModel userViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(userViewModel);
+            }
+
+            using (var dbContext = new Example001Entities())
+            {
+                var idState = (from s in dbContext.cstates where s.name.Equals("Active") select s.id).FirstOrDefault();
+
+                var oUser = new user()
+                {
+                    username = userViewModel.UserName,
+                    password = userViewModel.Password,
+                    name = userViewModel.Name,
+                    idState = idState
+                };
+
+                dbContext.users.Add(oUser);
+                dbContext.SaveChanges();
+            }
+
+            return Redirect(Url.Content("~/User/"));
         }
     }
 }
---
+++ Hector_de_Leon_Curso_de MVC_Net_CSharp/Exercise0001/Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,46 @@
+﻿namespace Exercise0001.Models.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.ComponentModel.DataAnnotations;
+
+    public class UserViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "User or E-mail")]
+        [StringLength(100, ErrorMessage = "The useror email contains more than {0} characters and less than {1}.", MinimumLength = 1)]
+        public string UserName
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The passwords are not equals.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
