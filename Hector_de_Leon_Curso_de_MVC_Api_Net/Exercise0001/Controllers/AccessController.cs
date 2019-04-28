f667ec7c344969fc6b1366ebeefca00a861ea2e1
Straad <estrada.meraz@gmail.com>
2019-04-28 18:21:04 -0500

    Authentication and access token was done in this step.

--- Hector_de_Leon_Curso_de_MVC_Api_Net/Exercise0001/Controllers/AccessController.cs
+++ Hector_de_Leon_Curso_de_MVC_Api_Net/Exercise0001/Controllers/AccessController.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-using Exercise0001.Models.WS;
-
-namespace Exercise0001.Controllers
+﻿namespace Exercise0001.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+    using Exercise0001.Models.WS;
+    using Exercise0001.Models;
+
     public class AccessController : ApiController
     {
         [HttpGet]
@@ -16,6 +17,44 @@ namespace Exercise0001.Controllers
             var oReplay = new Reply();
             oReplay.result = 1;
             oReplay.message = "Hellow World";
+
+            return oReplay;
+        }
+
+        [HttpPost]
+        public Reply Login([FromBody]AccessViewModel model)
+        {
+            Reply oReplay = new Reply();
+
+            try
+            {
+                using (var dbContext = new Example002Entities())
+                {
+                    var query = from u in dbContext.Users
+                                join s in dbContext.States on u.IdState equals s.Id
+                                where s.Name.Equals("Active") && u.Email.Equals(model.email) && u.Password.Equals(model.password)
+                                select u;
+
+                    if(query.Count()==1)
+                    {
+                        oReplay.result = 1;
+                        oReplay.data = Guid.NewGuid().ToString();
+
+                        User oUser = query.First();
+                        oUser.Token = oReplay.data.ToString();
+                        dbContext.Entry(oUser).State = System.Data.Entity.EntityState.Modified;
+                        dbContext.SaveChanges();
+                    }
+                    else
+                    {
+                        oReplay.message = "User's data are incorrect.";
+                    }
+                }
+            }
+            catch(Exception exception)
+            {
+                oReplay.message = string.Format("There was an error.", exception.Message);
+            }
 
             return oReplay;
         }
---
+++ Hector_de_Leon_Curso_de_MVC_Api_Net/Exercise0001/Models/WS/AccessViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Exercise0001.Models.WS
+{
+    public class AccessViewModel
+    {
+        public string email
+        {
+            get;
+            set;
+        }
+
+        public string password
+        {
+            get;
+            set;
+        }
+    }
+}
