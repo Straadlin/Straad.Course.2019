1b3e9d6c67beeffa67e329f92aaaf961d966f86c
Straad <estrada.meraz@gmail.com>
2019-05-05 22:35:46 -0500

    I made a service to be consumed in Home component.

--- Alberto_Picazo_Curso_Angular_7/Exercise_001/Exercise001/src/app/components/home/home.component.ts
+++ Alberto_Picazo_Curso_Angular_7/Exercise_001/Exercise001/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) {
+
+    console.log(this.userService.getUserName());
+  }
 
   ngOnInit() {
   }
