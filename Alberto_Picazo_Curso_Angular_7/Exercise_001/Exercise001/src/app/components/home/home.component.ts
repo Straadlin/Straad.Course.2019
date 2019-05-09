ae0249b2a1c9b131a15a7411324b3a1f667986d6
Straad <estrada.meraz@gmail.com>
2019-05-08 22:59:40 -0500

    Creating forms, binding them with models.

--- Alberto_Picazo_Curso_Angular_7/Exercise_001/Exercise001/src/app/components/home/home.component.ts
+++ Alberto_Picazo_Curso_Angular_7/Exercise_001/Exercise001/src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
-import { User } from '../../models/UserModel';
+import { FormControl, FormGroup, FormBuilder, FormArray } from '@angular/forms';
+import { User } from 'src/app/Models/UserModel';
 
 @Component({
   selector: 'app-home',
@@ -9,17 +10,27 @@ import { User } from '../../models/UserModel';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private userService: UserService) {
+  form: FormGroup;
 
-    const user = new User();
-    user.Email = 'alfredo.estrada@straad.mx';
-    user.FirstName = 'Alfredo';
-    user.LastName = 'Estrada';
-
-    this.userService.addUser(user);
+  constructor(private userService: UserService, private fb: FormBuilder) {
+    
   }
 
   ngOnInit() {
+
+    this.form = this.fb.group({
+      Email: [''],
+      FirstName: [''],
+      LastName: ['']
+    });
   }
 
+  onSubmit(formValue: any) {
+
+    const user = new User();
+    user.Email = formValue.Email;
+    user.FirstName = formValue.FirstName;
+    user.LastName = formValue.LastName;
+    this.userService.addUser(user);
+  }
 }
